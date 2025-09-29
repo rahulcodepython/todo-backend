7e7f3f7100eecce08c19a554a2563e5b4794bedb
rahulcodepython <rahulcodepython@gmail.com>
2025-09-30 00:32:43 +0530

    Logger & Recover Middleware are added and rest of them are renamed.

---
+++ backend/middleware/logger.go
@@ -0,0 +1,12 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func Logger() fiber.Handler {
+	return logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	})
+}
