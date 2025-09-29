1974afb5ab973c4bb2710be871c9c21505851adf
rahulcodepython <rahulcodepython@gmail.com>
2025-09-30 00:20:57 +0530

    Grace full shut down of the server using signals
    The code snippit is taken from official github recipes of Fiber
    Site: https://github.com/gofiber/recipes

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rahulcodepython/todo-backend/backend/config"
@@ -12,12 +15,27 @@ import (
 func main() {
 	cfg := config.LoadConfig()
 
-	database.ConnectDB(cfg)
+	db := database.ConnectDB(cfg)
 
 	server := fiber.New()
 
 	address := fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port)
-	if err := server.Listen(address); err != nil {
-		log.Panicf("Server error: %v", err)
-	}
+
+	go func() {
+		if err := server.Listen(address); err != nil {
+			log.Panicf("Server error: %v", err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	fmt.Println("Gracefully shutting down...")
+	_ = server.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+	_ = db.Close()
+
+	fmt.Println("Fiber was successful shutdown.")
 }
