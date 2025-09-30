efd74c83f8cc97886e2ae3d86ccdd139e4d5e055
rahulcodepython <rahulcodepython@gmail.com>
2025-09-30 19:20:39 +0530

    All tables are created at initial stage.

--- backend/database/db.go
+++ backend/database/db.go
@@ -19,6 +19,50 @@ func PingDB(db *sql.DB) {
 	log.Println("Database is healthy.")
 }
 
+func createTable(db *sql.DB) {
+	var query string
+
+	// JWT Token Table
+	query = `
+		CREATE TABLE IF NOT EXISTS jwt_tokens (
+		id UUID PRIMARY KEY,
+		token TEXT NOT NULL UNIQUE,
+		expires_at TIMESTAMPTZ NOT NULL,
+		created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+		);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Println("Unable to create jwt token table")
+		log.Fatal(err)
+	}
+	log.Println("jwt_tokens table created successfully.")
+
+	// User Table
+	query = `
+		CREATE TABLE IF NOT EXISTS users (
+		id UUID PRIMARY KEY,
+		name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		image TEXT,
+		password TEXT NOT NULL,
+		jwt UUID UNIQUE,
+		created_at TIMESTAMPTZ NOT NULL,
+		updated_at TIMESTAMPTZ NOT NULL,
+		CONSTRAINT fk_jwt
+			FOREIGN KEY(jwt)
+			REFERENCES jwt_tokens(id)
+			ON DELETE SET NULL
+	);
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Println("Unable to create user table")
+		log.Fatal(err)
+	}
+	log.Println("users table created successfully.")
+}
+
 func ConnectDB(cfg *config.Config) *sql.DB {
 	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", cfg.Database.DBHost, cfg.Database.DBPort, cfg.Database.DBUser, cfg.Database.DBPassword, cfg.Database.DBName, cfg.Database.DBSSLMode)
 
@@ -29,6 +73,7 @@ func ConnectDB(cfg *config.Config) *sql.DB {
 	}
 
 	PingDB(db)
+	createTable(db)
 
 	return db
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require (
 	github.com/gofiber/fiber/v2 v2.52.9
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.14.0
@@ -12,7 +13,6 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
